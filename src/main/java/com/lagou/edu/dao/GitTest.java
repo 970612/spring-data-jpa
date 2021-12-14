0b3507e4f2ab1df8d81769a7d3a92a13e1944b5d
501566581 <501566581@qq.com>
2021-12-14 20:11:40 +0800

    push commit

--- src/main/java/com/lagou/edu/dao/GitTest.java
+++ src/main/java/com/lagou/edu/dao/GitTest.java
@@ -7,5 +7,9 @@ public class GitTest {
         System.out.println("hello Git1");
         System.out.println("hello Master");
         System.out.println("hello Git6");
+        System.out.println("hello Git6");
+        System.out.println("hello push");
+
+
     }
 }
