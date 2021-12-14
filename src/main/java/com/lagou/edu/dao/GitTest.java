6078add330f21ff3ac5cac0dc29a6257d49f5343
501566581 <501566581@qq.com>
2021-12-14 19:37:52 +0800

    second commit

--- src/main/java/com/lagou/edu/dao/GitTest.java
+++ src/main/java/com/lagou/edu/dao/GitTest.java
@@ -3,5 +3,6 @@ package com.lagou.edu.dao;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("hello Git");
     }
 }
